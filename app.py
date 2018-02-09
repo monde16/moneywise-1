7d2887873e7915f2dea54cb2cccd85ad726d3e4b
Monde Ngalonkulu <monde.ngalo@gmail.com>
2018-02-10 01:45:46 +0200

    Incorporate holidays config file

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 from calendar import monthrange, weekday
-from datetime import datetime, date, time, timedelta
+from datetime import datetime, date, timedelta
 import argparse
 import re
+import config
 
 
 def get_args():
@@ -15,15 +16,26 @@ def get_args():
 
 
 def str2date(s):
-    if re.match(r'\d\d\d\d[^\d]\d\d?[^\d]\d\d?', s):
+    if re.match(r'(?:\d{4}([^\d])\d\d?\1\d\d?)', s):
         t = tuple([int(e) for e in s.split(s[4])])
         return date(*t)
     else:
         raise ValueError(f'{s} is not of fomat: yyyy/mm/dd')
 
 
+def get_holidays():
+    return [str2date(t[0]) for t in config.holidays]
+
+
+holidays = get_holidays()
+
+
 def is_weekday(date):
     return weekday(date.year, date.month, date.day) in range(5)
+
+
+def is_holiday(date):
+    return date in holidays
 
 
 def last_month_date(dt):
@@ -32,8 +44,8 @@ def last_month_date(dt):
     return date(yy, mm, dd)
 
 
-def weekdays(a, b):
-    """Calculate weekdays between dates a & b.
+def workdays(a, b):
+    """Calculate workdays between dates a & b.
 
     The calc is right exclusive. Thus [a, b), much like range() and other similar methods
     """
@@ -42,17 +54,18 @@ def weekdays(a, b):
     while a + td*n < b:
         day = a + td * n
         n += 1
-        if is_weekday(day):
+        if is_weekday(day) and not is_holiday(day):
             yield day
 
 
 def main():
     args = get_args()
-    
+
     first = str2date(args.first) if args.first else datetime.now().date()
     last = str2date(args.last) if args.last else last_month_date(first)
-    days = len(list(weekdays(first, last + timedelta(1)))) # here we go past the last day so that the weekdays method includes it.
-    print(f'{first} .. {last} => {days} week days')
+    days = len(list(workdays(first, last + timedelta(1)))) # here we go past the last day so that the workdays method includes it.
+    print(f'{first} .. {last} => {days} work days')
+    print(f"Holidays:")
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,17 @@
+holidays = [
+    ('2018-01-01', "New Year’s Day"),
+    ('2018-03-21', "Human Rights Day"),
+    ('2018-03-30', "Good Friday"),
+    ('2018-04-02', "Family Day"),
+    ('2018-04-27', "Freedom Day"),
+    ('2018-04-30', "School Holiday"),
+    ('2018-05-01', "Workers’ Day"),
+    ('2018-06-16', "Youth Day"),
+    ('2018-08-09', "National Women’s Day"),
+    ('2018-08-10', "School Holiday"),
+    ('2018-09-24', "Heritage Day"),
+    ('2018-12-16', "Day of Reconciliation"),
+    ('2018-12-17', "Public Holiday"),
+    ('2018-12-25', "Christmas Day"),
+    ('2018-12-26', "Day of Goodwill"),
+]
